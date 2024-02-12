3fd38eceed2204f3589ad8864a7fc6af22a96311
baaass2 <jokerbass087@gmail.com>
2024-02-12 17:00:04 -0300

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 
-module.exports = {
-  important: true,
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
+/* @type {import('tailwindcss').Config} */
+module.exports = { 
+  content: [ 
+    "./app/**/*.{js,ts,jsx,tsx}", 
+    "./pages/**/*.{js,ts,jsx,tsx}", 
+    "./components/**/*.{js,ts,jsx,tsx}", 
+      
+    // Or if using `src` directory: 
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ], 
+  theme: { 
+    extend: {}, 
+  }, 
+  plugins: [], 
+}
